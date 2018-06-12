857f4393eb4d5999183e00ca637c479d8ddddb0e
pluto-the-lost <gaohx@users.noreply.github.com>
2018-06-13 00:22:47 +0800

    problem 66-plus one

---
+++ solutions/day2_prob66_plus_one.py
@@ -0,0 +1,42 @@
+'''
+link: https://leetcode.com/problems/plus-one/description/
+
+Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+
+The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+
+You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+Example 1:
+
+Input: [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+Example 2:
+
+Input: [4,3,2,1]
+Output: [4,3,2,2]
+Explanation: The array represents the integer 4321.
+'''
+
+#solution:
+class Solution:
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        if digits[-1] != 9:
+            digits[-1] += 1
+            return(digits)
+        else:
+            digits[-1] = 0
+            carryCursor = -2
+            while(-carryCursor<=len(digits)):
+                if digits[carryCursor] != 9:
+                    digits[carryCursor] += 1
+                    return(digits)
+                else:
+                    digits[carryCursor] = 0
+                    carryCursor -= 1
+            return([1]+digits)
